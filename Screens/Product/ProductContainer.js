34edc8097999498441213c164367290f09d9a64c
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-07 08:48:15 +0800

    product card

--- Screens/Product/ProductContainer.js
+++ Screens/Product/ProductContainer.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet,FlatList, ActivityIndicator } from 'react-native'
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, ScrollView } from 'react-native'
 
 import ProductList from './ProductList'
 
-const data =require('../../assets/data/products.json')
+const data = require('../../assets/data/products.json')
 
 const ProductContainer = () => {
     const [products, setProducts] = useState([])
@@ -15,27 +15,30 @@ const ProductContainer = () => {
     }, [])
 
     return (
-        <View>
-        <Text>Product Container</Text>
-        <View style={{ marginTop: 200}} >
+        <ScrollView>
+            <Text>Product Container</Text>
+            <View style={{ marginTop: 200 }} >
             <FlatList 
-                horizontal
-                data={products}
-                renderItem={({item}) => <ProductList key={item.id} item={item}/>}
-                keyExtractor={item => item.name}
-            />
-        </View>
-    </View> 
+                //    horizontal
+                   columnWrapperStyle={{justifyContent: 'space-between'}}
+                   numColumns={2}
+                    data={products}
+                   
+                    renderItem={({item}) => <ProductList key={item.id} item={item}/>}
+                    keyExtractor={item => item.name}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+});
 
 export default ProductContainer;
