818b7239260e3936b0b2149a7726476118ac0aad
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-07 09:35:19 +0800

    search

--- Screens/Product/SearchedProduct.js
+++ Screens/Product/SearchedProduct.js
@@ -1,74 +1,68 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions} from 'react-native'
-import { Container, 
-    VStack, 
-    Input, 
-    Heading, 
-    Text, 
-    Icon, 
+import { View, StyleSheet, Dimensions, FlatList } from 'react-native'
+import {
+    Container,
+    VStack,
+    Input,
+    Heading,
+    Text,
+    Icon,
     HStack,
     Box,
     Avatar,
     Spacer,
-     } from "native-base";
-import { FlatList } from 'react-native';
+} from "native-base";
+
 
 var { width } = Dimensions.get("window")
 
 const SearchedProduct = (props) => {
     const { productsFiltered } = props;
-    return(
-        
-            <Container style={{ width: width }}>
-                {productsFiltered.length > 0 ? (
-                    // productsFiltered.map((item) => (
-                    //      console.log(item)
-                        
-                       
-                    // ))
-        <Box width={80}>
-            <Heading fontSize="xl" p="4" pb="3">
-                Inbox
-            </Heading>
-            <FlatList data={productsFiltered} renderItem={({item}) => 
-                <Box borderBottomWidth="1" _dark={{
-                    borderColor: "muted.50"
-                    }} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
+    return (
+        <Container style={{ width: width }}>
+            {productsFiltered.length > 0 ? (
+                <Box width={80}>
+                    <FlatList 
+                    data={productsFiltered} 
+                    renderItem={({ item }) =>
+                        <Box borderBottomWidth="1" _dark={{
+                            borderColor: "muted.50"
+                        }} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
                             <HStack space={[2, 3]} justifyContent="space-between">
-                            <Avatar size="48px" source={{
-                        uri: item.image ? 
-                        item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
-                        }} />
-                            <VStack>
-                                <Text _dark={{
-                            color: "warmGray.50"
-                        }} color="coolGray.800" bold>
-                                {item.name}
-                                </Text>
-                                <Text color="coolGray.600" _dark={{
-                            color: "warmGray.200"
-                        }}>
-                                {item.description}
-                                </Text>
-                            </VStack>
-                            <Spacer />
-                            <Text fontSize="xs" _dark={{
-                                color: "warmGray.50"
+                                <Avatar size="48px" source={{
+                                    uri: item.image ?
+                                        item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                                }} />
+                                <VStack>
+                                    <Text _dark={{
+                                        color: "warmGray.50"
+                                    }} color="coolGray.800" bold>
+                                        {item.name}
+                                    </Text>
+                                    <Text color="coolGray.600" _dark={{
+                                        color: "warmGray.200"
+                                    }}>
+                                        {item.description}
+                                    </Text>
+                                </VStack>
+                                <Spacer />
+                                <Text fontSize="xs" _dark={{
+                                    color: "warmGray.50"
                                 }} color="coolGray.800" alignSelf="flex-start">
-                                        {item.price}
-                            </Text>
+                                    {item.price}
+                                </Text>
                             </HStack>
                         </Box>} keyExtractor={item => item._id} />
-        </Box>
-                ) : (
-                    <View style={styles.center}>
-                        <Text style={{ alignSelf:  'center' }}>
-                            No products match the selected criteria
-                        </Text>
-                    </View>
-                )}
-            </Container>
-       
+                </Box>
+            ) : (
+                <View style={styles.center}>
+                    <Text style={{ alignSelf: 'center' }}>
+                        No products match the selected criteria
+                    </Text>
+                </View>
+            )}
+        </Container>
+
     );
 };
 
@@ -86,7 +80,7 @@ const styles = StyleSheet.create({
         alignItems: "flex-start",
         flexWrap: "wrap",
         backgroundColor: "gainsboro",
-      },
+    },
 })
 
 export default SearchedProduct;
