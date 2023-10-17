4b2098e00577ac64b586fba79cf49a9ce6b93a3b
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-17 13:43:08 +0800

    cart icon

--- Navigators/Main.js
+++ Navigators/Main.js
@@ -5,7 +5,7 @@ import { View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import HomeNavigator from "./HomeNavigator";
 import Cart from "../Screens/Cart/Cart";
-// import CartIcon from "../Shared/CartIcon";
+import CartIcon from "../Shared/CartIcon";
 
 const Tab = createBottomTabNavigator();
 
@@ -49,7 +49,7 @@ const Main = () => {
                                 size={30}
 
                             />
-                            {/* <CartIcon /> */}
+                            <CartIcon />
                         </>)
                     }
                 }}
