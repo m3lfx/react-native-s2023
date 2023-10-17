7f5701f9473ac0de1965afd9bf404d5a98b3ce14
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-17 15:48:57 +0800

    register

--- Screens/User/Register.js
+++ Screens/User/Register.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
+import { View, Text, StyleSheet,  } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from '@react-navigation/native';
+import { Button } from "native-base";
+import FormContainer from "../../Shared/Form/FormContainer";
+import Input from "../../Shared/Form/Input";
+import Error from "../../Shared/Error";
 
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+const Register = (props) => {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigation = useNavigation()
 
-export default Register
+    const register = () => {
+        if (email === "" || name === "" || phone === "" || password === "") {
+            setError("Please fill in the form correctly");
+        }
+    }
+
+    return (
+        <KeyboardAwareScrollView
+            viewIsInsideTabBar={true}
+            extraHeight={200}
+            enableOnAndroid={true}
+        >
+            <FormContainer title={"Register"}>
+                <Input
+                    placeholder={"Email"}
+                    name={"email"}
+                    id={"email"}
+                    onChangeText={(text) => setEmail(text.toLowerCase())}
+                />
+                <Input
+                    placeholder={"Name"}
+                    name={"name"}
+                    id={"name"}
+                    onChangeText={(text) => setName(text)}
+                />
+                <Input
+                    placeholder={"Phone Number"}
+                    name={"phone"}
+                    id={"phone"}
+                    keyboardType={"numeric"}
+                    onChangeText={(text) => setPhone(text)}
+                />
+                <Input
+                    placeholder={"Password"}
+                    name={"password"}
+                    id={"password"}
+                    secureTextEntry={true}
+                    onChangeText={(text) => setPassword(text)}
+                />
+                <View style={styles.buttonGroup}>
+                    {error ? <Error message={error} /> : null}
+                </View>
+                <View>
+                    <Button variant={"ghost"} onPress={() => register()}>
+                        <Text style={{ color: "blue" }}>Register</Text>
+                    </Button>
+                </View>
+                <View>
+                    <Button variant={"ghost"}
+                        onPress={() => navigation.navigate("Login")}
+                    >
+                        <Text style={{ color: "blue" }}>Back to Login</Text>
+                    </Button>
+                </View>
+            </FormContainer>
+        </KeyboardAwareScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    buttonGroup: {
+        width: "80%",
+        margin: 10,
+        alignItems: "center",
+    },
+});
+
+export default Register;
