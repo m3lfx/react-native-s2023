7f83e5ed32e56a6b6069264338c7a47f5c7d3ddb
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-17 13:50:34 +0800

    checkout navigator

---
+++ Screens/Cart/Checkout/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Checkout = () => {
+  return (
+   <Text>Checkout</Text>
+  )
+}
+
+export default Checkout
---
+++ Screens/Cart/Checkout/Confirm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const Confirm = () => {
+  return (
+   <Text>Checkout</Text>
+  )
+}
+
+export default Confirm
