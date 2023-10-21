cd2efc136b3c831a3d01eaa12111c78a02847854
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-21 08:31:59 +0800

    user profile

--- assets/common/baseurl.js
+++ assets/common/baseurl.js
@@ -4,7 +4,7 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.230.215:4000/api/v1/'
+? baseURL = 'http://172.34.99.109:4000/api/v1/'
 : baseURL = 'http://192.168.1.11:4000/api/v1/'
 }
 
